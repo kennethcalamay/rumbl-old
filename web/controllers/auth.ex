4c17633d75e3f070b5ee6d0ea8962bac44ae2608
KL <git@kennethcalamay.com>
2015-10-02 20:09:22 +0800

    Signs in newly signed up users

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -1,4 +1,4 @@
-defmodule Rumble.Auth do
+defmodule Rumbl.Auth do
   import Plug.Conn
 
   def init(opts) do
@@ -9,5 +9,12 @@ defmodule Rumble.Auth do
     user_id = get_session(conn, :user_id)
     user    = user_id && repo.get(Rumbl.User, user_id)
     assign(conn, :current_user, user)
+  end
+
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
   end
 end
